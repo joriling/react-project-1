c776f93f3a8c33a99be4901b0e6e4662092040fe
Joriling <jollyrilakkuma@gmail.com>
2023-06-21 19:59:21 +0800

    update

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -14,7 +14,7 @@ const MainNavigation = () => {
             <Link to="/"> All Meetups</Link>
           </li>
           <li>
-            <Link to="/new-meetup"> Add New Meetup</Link>
+            <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
             <Link to="/favorites">
